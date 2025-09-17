6d6df6aadbc6d16e44037ee8a424aba34cbee79e
CesarEmiliano79 <cesaremi12rjpz@gmail.com>
2025-09-17 00:29:33 -0600

    Start.js funcional

--- start.js
+++ start.js
@@ -1,11 +1,13 @@
-import fs from "fs"
-import readline from "readline"
+import fs from "fs";
+import readline from "readline";
 
-const rl = readline.createInterface({input:process.stdin, output: process.stdout})
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-rl.question('Cual es tu MONGODB URI',(URIMONGO)=>{
-    fs.writeFileSync('.env.local', 'MONGODB_URI=${URIMONGO}')
-})
-
-
-
+rl.question("Cual es tu MONGODB URI: ", (URIMONGO) => {
+  fs.writeFileSync(".env.local", `MONGODB_URI=${URIMONGO}\n`);
+  console.log("Archivo .env.local creado con éxito");
+  rl.close();
+});
